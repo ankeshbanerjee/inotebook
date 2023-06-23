e2bad8c1c7333ee9d44ea831e2286b43c3441d76
ankeshbanerjee <banerjeeankesh@gmail.com>
2023-06-24 01:12:52 +0530

    tested useContext api for state management

--- src/context/notes/NoteContext.js
+++ src/context/notes/NoteContext.js
@@ -3,3 +3,6 @@ import { createContext } from "react";
 const NoteContext = createContext();
 
 export default NoteContext
+
+// # State management - sharing states among all react components using context api (useConext hook)
+// https://www.geeksforgeeks.org/how-to-share-state-across-react-components-with-context/
